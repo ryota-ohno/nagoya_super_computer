bb7405947fcfff3817cc5ac3c8f88792a37c6d34
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2022-11-16 14:30:21 +0900

    Add files via upload

--- polyacene/step1+para/src/step1_8_xyz_twist_2.py
+++ polyacene/step1+para/src/step1_8_xyz_twist_2.py
@@ -204,7 +204,7 @@ def get_opt_params_dict(df_cur, init_params_dict,fixed_params_dict):
         para_list=[]
         for a in [a_init_prev-0.05,a_init_prev,a_init_prev+0.05]:
             for b in [b_init_prev-0.05,b_init_prev,b_init_prev+0.05]:
-                a = np.round(a,1);b = np.round(b,1)
+                a = np.round(a,2);b = np.round(b,2)
                 df_val_ab = df_val[
                     (df_val['a']==a)&(df_val['b']==b)&(df_val['theta']==theta)&
                     (df_val['Rt']==Rt)&(df_val['Rp']==Rp)&(df_val['A1']==A1)&(df_val['A2']==A2)&
