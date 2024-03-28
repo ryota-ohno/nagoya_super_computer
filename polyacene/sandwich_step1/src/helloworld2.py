80bea8c622a85f9e909c8e8573e3c1e905a7f419
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2024-03-28 17:56:17 +0900

    Add files via upload

---
+++ polyacene/sandwich_step1/src/helloworld2.py
@@ -0,0 +1,15 @@
+##tetracene層内計算
+import os
+os.environ['HOME'] ='/data/group1/z40145w'
+import pandas as pd
+import time
+import numpy as np
+from scipy import signal
+import scipy.spatial.distance as distance
+import random
+
+print('Hello World 1')
+time.sleep(20)
+print('Hello World 2')
+time.sleep(20)
+print('Hello World 3')
