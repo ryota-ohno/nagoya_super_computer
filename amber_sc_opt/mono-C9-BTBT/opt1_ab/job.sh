9873ccd5553fb1b52ffdb1d1b1e93d821ca54fad
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2025-08-08 13:52:21 +0900

    Add files via upload

---
+++ amber_sc_opt/mono-C9-BTBT/opt1_ab/job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PJM -L "rscunit=fx"
+#PJM -L "rscgrp=fx-small"
+#PJM -L "node=1"
+#PJM -L "elapse=24:00:00"
+#PJM -j
+#PJM -S
+#PJM "--norestart"
+
+python /data/group1/z40145w/Working/nagoya_super_computer/amber_sc_opt/mono-C9-BTBT/src/step1_8_xyz_phi2.py --auto-dir opt1_phi --monomer-name mono-C9-BTBT --num-nodes 5 
