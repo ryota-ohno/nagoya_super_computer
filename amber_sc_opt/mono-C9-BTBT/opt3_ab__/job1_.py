ab9adbb61497dd81ce79776227e0249181a34583
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2025-08-18 16:51:30 +0900

    Add files via upload

---
+++ amber_sc_opt/mono-C9-BTBT/opt3_ab__/job1_.py
@@ -0,0 +1,75 @@
+##tetracene層内計算
+import os
+os.environ['HOME'] ='/data/group1/z40145w'
+import pandas as pd
+import argparse
+import subprocess
+
+def init_process(args):
+    auto_dir = f'/data/group1/z40145w/Working/nagoya_super_computer/amber_sc_opt/mono-C9-BTBT/{args.auto_dir}'
+    df_init=pd.read_csv(os.path.join(auto_dir,'step1_init_params_.csv'))
+    
+    for idx, row in df_init.iterrows():
+        params_dict_1 = row[['theta','A2','z']].to_dict()
+        params_dict_2 = row[['a','b','phi']].to_dict()
+        dir_name = ''
+        for key,val in params_dict_1.items():
+            dir_name += '{}_'.format(val)
+        path_dir=os.path.join(auto_dir,f'{dir_name}')
+        if os.path.exists(os.path.join(path_dir,'done.txt')):
+            continue
+        subprocess.run(['pjsub',os.path.join(auto_dir,f'{dir_name}/job.sh')])
+
+def main_process(args):
+    auto_dir = f'/data/group1/z40145w/Working/nagoya_super_computer/amber_sc_opt/mono-C9-BTBT/{args.auto_dir}'
+    df_init=pd.read_csv(os.path.join(auto_dir,'step1_init_params_.csv'))
+    inprogress = True
+    while inprogress:
+        for idx, row in df_init.iterrows():
+            params_dict_1 = row[['theta','A2','z']].to_dict()
+            dir_name = ''
+            for key,val in params_dict_1.items():
+                dir_name += '{}_'.format(val)
+            path_dir=os.path.join(auto_dir,f'{dir_name}')
+            if os.path.exists(os.path.join(path_dir,'done.txt')):
+                df_init = update_value_in_df(df_init,idx,'status','Done')
+                df_init.to_csv(os.path.join(auto_dir,'step1_init_params_.csv'),index=False)
+        df_init_done=df_init[df_init['status']=='Done']
+        if len(df_init)==len(df_init_done):
+            inprogress = False
+    print('Done')
+
+def result_process(args):
+    subprocess.run(['rm','*sh.*'])
+    auto_dir = f'/data/group1/z40145w/Working/nagoya_super_computer/amber_sc_opt/mono-C9-BTBT/{args.auto_dir}'
+    df_init=pd.read_csv(os.path.join(auto_dir,'step1_init_params_.csv'))
+    df_tot=[]
+    for idx, row in df_init.iterrows():
+        params_dict_1 = row[['theta','A2','z']].to_dict()
+        dir_name = ''
+        for key,val in params_dict_1.items():
+            dir_name += '{}_'.format(val)
+        path_dir=os.path.join(auto_dir,f'{dir_name}')
+        df=pd.read_csv(os.path.join(path_dir,'step1.csv'))
+        df_tot.append(df)
+    df_=pd.concat(df_tot, ignore_index=True)
+    df_.to_csv(os.path.join(auto_dir,'step1.csv'),index=False)
+
+def update_value_in_df(df,index,key,value):
+    df.loc[index,key]=value
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--isTest',action='store_true')
+    parser.add_argument('--auto-dir',type=str,help='path to dir which includes gaussian, gaussview and csv')
+    ##maxnum-machine2 がない
+    args = parser.parse_args()
+
+    print("----main process----")
+    init_process(args)
+    main_process(args)
+    result_process(args)
+    print("----finish process----")
+    
