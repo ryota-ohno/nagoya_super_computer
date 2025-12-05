ef21949b3f5c113433721d8b88f36891c01e3c3c
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2025-12-05 18:07:16 +0900

    Add files via upload

--- amber_sc_opt/pentacene/step3_para/pentacene_test_new/result.py
+++ amber_sc_opt/pentacene/step3_para/pentacene_test_new/result.py
@@ -8,7 +8,8 @@ import subprocess
 def main_process(args):
     auto_dir = f'/data/group1/z40145w/Working/nagoya_super_computer/amber_sc_opt/pentacene/step3_para/{args.auto_dir}'
     df_init=pd.read_csv(os.path.join(auto_dir,'step3_init_params.csv'))
-    while inprogress:
+    
+    while True:
         for i in range(75):
             cx=round(-3.7+i*0.1,1)
             dir_name = f'{cx}'
@@ -19,7 +20,7 @@ def main_process(args):
                 df_init.to_csv(os.path.join(auto_dir,'step3_init_params.csv'),index=False)
         df_init_done=df_init[df_init['status']=='Done']
         if len(df_init)==len(df_init_done):
-            inprogress = False
+            break
     print('Done')    
 
 def result_process(args):
