74cbc385484dcda9fe2127ebf071f0033f378b3d
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2025-12-13 09:51:02 +0900

    Add files via upload

---
+++ amber_sc_opt/pentacene/step3_para/pentacene_test_new_new_/result.py
@@ -0,0 +1,56 @@
+##tetracene層内計算
+import os
+os.environ['HOME'] ='/data/group1/z40145w'
+import pandas as pd
+import argparse
+import subprocess
+
+def main_process(args):
+    auto_dir = f'/data/group1/z40145w/Working/nagoya_super_computer/amber_sc_opt/pentacene/step3_para/{args.auto_dir}'
+    df_init=pd.read_csv(os.path.join(auto_dir,'step3_init_params.csv'))
+    
+    while True:
+        for i in range(75):
+            cx=round(-3.7+i*0.1,1)
+            dir_name = f'{cx}'
+            path_dir=os.path.join(auto_dir,f'{dir_name}')
+            df_init_=pd.read_csv(os.path.join(path_dir,'step3_init_params.csv'))
+            if len(df_init_)==len(df_init_[df_init_['status']=='Done']):
+                df_init.loc[df_init['cx'] == cx, 'status'] = 'Done'
+                df_init.to_csv(os.path.join(auto_dir,'step3_init_params.csv'),index=False)
+        df_init_done=df_init[df_init['status']=='Done']
+        if len(df_init)==len(df_init_done):
+            break
+    print('Done')    
+
+def result_process(args):
+    subprocess.run(['rm','*.sh.*'])
+    auto_dir = f'/data/group1/z40145w/Working/nagoya_super_computer/amber_sc_opt/pentacene/step3_para/{args.auto_dir}'
+    df_init=pd.read_csv(os.path.join(auto_dir,'step3_init_params.csv'))
+    df_tot=[]
+    for i in range(75):
+        cx=round(-3.7+i*0.1,1)
+        dir_name = f'{cx}'
+        path_dir=os.path.join(auto_dir,f'{dir_name}')
+        df=pd.read_csv(os.path.join(path_dir,'step3.csv'))
+        df_tot.append(df)
+    df_=pd.concat(df_tot, ignore_index=True)
+    df_.to_csv(os.path.join(auto_dir,'step3.csv'),index=False)
+    
+def update_value_in_df(df,index,key,value):
+    df.loc[index,key]=value
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--isTest',action='store_true')
+    parser.add_argument('--auto-dir',type=str,help='path to dir which includes gaussian, gaussview and csv')
+    ##maxnum-machine2 がない
+    args = parser.parse_args()
+
+    print("----main process----")
+    main_process(args)
+    result_process(args)
+    print("----finish process----")
+    
