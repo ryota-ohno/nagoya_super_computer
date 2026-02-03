f6d5a97d678d8c7e7157920cf6c47bef8c9701c7
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2026-02-03 17:05:22 +0900

    Add files via upload

---
+++ amber_sc_opt/ditBu-BTBT/test/job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PJM -L "rscunit=fx"
+#PJM -L "rscgrp=fx-small"
+#PJM -L "node=1"
+#PJM -L "elapse=1:00:00"
+#PJM -j
+#PJM -S
+#PJM "--norestart"
+
+python /data/group1/z40145w/Working/nagoya_super_computer/amber_sc_opt/ditBu_BTBT/src/step1_8_xyz_0_.py --auto-dir test --monomer-name ditBu_BTBT --num-nodes 7
